ef4fade9ca86d41e52930c7c50ae0d6ac7dcd5c0
JohnnyUFT <johnnyuft@gmail.com>
2017-10-25 18:41:20 -0300

    criado método de leitura de arquivo

--- arquivo.py
+++ arquivo.py
@@ -6,4 +6,19 @@ email: johnnyuft@gmail.com
 last modified: Outubro 2017
 '''
 
-import 
+import numpy as np
+import pandas as pd
+
+def leArquivo():
+    # lê o arquivo:
+    fin = open('arquivo', 'rt' )
+    while True:
+        linha = fin.readline()
+        if not linha:
+            break
+        trataLinhas(linha)
+    fin.close()
+
+# Define o que acontece com cada linha lida do arquivo;
+def trataLinhas(linha):
+    pass
